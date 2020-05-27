fc85988d96a5b19057ec3243daff013e73843412
Phil Moir <pgmoir@gmail.com>
2020-05-27 15:38:52 +0100

    Switch to new button example

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -1,18 +1,14 @@
-import React from 'react';
+import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { Button } from '@storybook/react/demo';
+import * as React from 'react';
+import Button from "./Button";
 
-export default {
-  title: 'Button',
-  component: Button,
-};
-
-export const Text = () => <Button onClick={action('clicked')}>Hello Syed Button</Button>;
-
-export const Emoji = () => (
-  <Button onClick={action('clicked')}>
-    <span role="img" aria-label="so cool">
-      😀 😎 👍 💯
-    </span>
-  </Button>
-);
+storiesOf("Button", module)
+  .add("with text", () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
+  ))
+  .add("with some emoji", () => (
+    <Button onClick={action('clicked')}>
+      <span role="img" aria-label="so cool">😀 😎 👍 💯</span>
+    </Button>
+  ));
